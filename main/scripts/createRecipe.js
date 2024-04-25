4d5ff1962f9f5982b2b69d6024e051c32da9fe0b
rhit-mackeycb <mackeycb@rose-hulman.edu>
2024-04-24 20:41:11 -0400

    half way with create single recipes page

---
+++ main/scripts/createRecipe.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector("#saveRecipeButton");
+btn.addEventListener("click", function() {
+let name = document.getElementById('nameV').value;
+let diff = document.getElementById('diffV').value;
+let serve = document.getElementById('serveV').value;
+let time = document.getElementById('timeV').value;
+let steps = document.getElementById('stepsV').value;
+let image = document.getElementById('imageV').value;
+console.log(name);
+console.log(diff);
+console.log(serve);
+console.log(time);
+console.log(steps);
+console.log(image);
+let obj = {nameV:name, diffV:diff, serveV: serve, timeV:time, stepsV:steps, imageV:image};
+console.log(obj.nameV);
+console.log(obj.diffV);
+console.log(obj.serveV);
+console.log(obj.timeV);
+console.log(obj.stepsV);
+console.log(obj.imageV);
+});
