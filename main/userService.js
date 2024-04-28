9914af091e3cbc46fd6f3bb5169cca9898d44e8f
Sparky-4 <anandy@rose-hulman.edu>
2024-04-28 17:05:42 -0400

    started all recipes page

--- main/userService.js
+++ main/userService.js
@@ -1,12 +1,12 @@
-const crypto = require('crypto')
+const crypto = require('crypto');
 const SHA256 = require("crypto-js/sha256");
 module.exports = {hashPassword, getSalt};
 
 function hashPassword(password){
-    const hash = SHA256(password).toString()
+    const hash = SHA256(password).toString();
     return hash;
 }
 
 function getSalt(){
-    return crypto.randomBytes(Math.ceil(16 / 2)).toString('hex').slice(0, 16);
+    return crypto.randomBytes(8).toString('hex').slice(0, 16);
 }
