9ce5a51a393d07981836a1e82e04df783b8ae19b
rhit-mackeycb <mackeycb@rose-hulman.edu>
2024-04-28 20:26:04 -0400

    difficulty dropdown and remove text when submited recipe

--- main/public/scripts/addRecipePageController.js
+++ main/public/scripts/addRecipePageController.js
@@ -1,21 +1,44 @@
 class AddRecipePageController {
     constructor() {
-        let btn = document.querySelector("#saveRecipeButton");
+        let diffControl = document.querySelector('#diffValue');
+
+        //Create array of options to be added
+        let diffArray = [1, 2, 3, 4, 5];
+
+        //Create and append select list
+        let diffList = document.createElement('select');
+        diffList.setAttribute("id", "mySelect");
+        diffControl.appendChild(diffList);
+
+        //Create and append the options
+        for (let i = 0; i < diffArray.length; i++) {
+            let diffOption = document.createElement('option');
+            diffOption.setAttribute("value", diffArray[i]);
+            diffOption.text = diffArray[i];
+            diffList.appendChild(diffOption);
+        }
+        let btn = document.querySelector('#saveRecipeButton');
         console.log("addrecipepagecontroller");
         btn.addEventListener("click", function () {
             console.log("button");
-            let name = document.getElementById('nameV').value;
-            let diff = document.getElementById('diffV').value;
-            let serve = document.getElementById('serveV').value;
-            let hours = document.getElementById('hoursV').value;
-            let minutes = document.getElementById('minutesV').value;
-            let steps = document.getElementById('stepsV').value;
-            let image = document.getElementById('imageV').value;
+            let name = document.querySelector('#nameV').value;
+            let diff = diffList.value;
+            let serve = document.querySelector('#serveV').value;
+            let hours = document.querySelector('#hoursV').value;
+            let minutes = document.querySelector('#minutesV').value;
+            let steps = document.querySelector('#stepsV').value;
+            let image = document.querySelector('#imageV').value;
             let time = hours + ":" + minutes + ":00";
-            console.log(rhit.auth.user);
             let user = rhit.auth.user.username;
 
             let obj = { nameV: name, diffV: diff, serveV: serve, timeV: time, stepsV: steps, imageV: image, userV: user };
+            document.querySelector('#nameV').value = '';
+            document.querySelector('#serveV').value = '';
+            document.querySelector('#hoursV').value = '';
+            document.querySelector('#minutesV').value = '';
+            document.querySelector('#stepsV').value = '';
+            document.querySelector('#imageV').value = '';
+            console.log(name + ' bruh');
             fetch('/addSingleRecipe', {
                 method: 'POST',
                 headers: {
